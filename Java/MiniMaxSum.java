005dc03021ddac80aa0f94163b985a39be10b1f5
BrunoRijo <bruno_rijo@hotmail.com>
2022-08-15 23:53:22 -0300

    MiniMaxSum - Java

---
+++ Java/MiniMaxSum.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void miniMaxSum(List<Integer> arr) {
+    Collections.sort(arr);
+    Long mininum = arr.stream().limit(arr.size()-1).mapToLong(i->i).sum();
+    arr.remove(0);
+    Long maximum = arr.stream().mapToLong(i-> i).sum();
+    
+    System.out.println(mininum+" "+maximum);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
+
